f7d4c0650c9dca1aa7e7b62215a14265fad6a8d4
Anjali853 <anjalianjali06149@gmail.com>
2026-08-30 16:27:02 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,29 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.OutputStream;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
 
-    try {
-      serverSocket = new ServerSocket(port);
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+
       serverSocket.setReuseAddress(true);
 
-      clientSocket = serverSocket.accept();
+      Socket clientSocket = serverSocket.accept();
+
+      OutputStream output = clientSocket.getOutputStream();
+
+      output.write("+PONG\r\n".getBytes());
+      output.flush();
+
+      clientSocket.close();
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
   }
 }
